92c423213edb45684fbe36c55c65a52f95d8b5de
Philip William Ventura <philipwilliamventura@gmail.com>
2025-07-31 19:13:47 -0400

    play 2048

--- main.c
+++ main.c
@@ -12,19 +12,19 @@
 // Trap Codes
 enum
 {
-    TRAP_GETC = 0x20,  /* get character from keyboard, not echoed onto the terminal */
-    TRAP_OUT = 0x21,   /* output a character */
-    TRAP_PUTS = 0x22,  /* output a word string */
-    TRAP_IN = 0x23,    /* get character from keyboard, echoed onto the terminal */
-    TRAP_PUTSP = 0x24, /* output a byte string */
-    TRAP_HALT = 0x25   /* halt the program */
+    TRAP_GETC = 0x20,  // get character from keyboard, not echoed onto the terminal
+    TRAP_OUT = 0x21,   // output a character
+    TRAP_PUTS = 0x22,  // output a word string
+    TRAP_IN = 0x23,    // get character from keyboard, echoed onto the terminal
+    TRAP_PUTSP = 0x24, // output a byte string
+    TRAP_HALT = 0x25   // halt the program
 };
 
 // Memory mapped registers
 enum
 {
-    MR_KBSR = 0xFE00, /* keyboard status */
-    MR_KBDR = 0xFE02  /* keyboard data */
+    MR_KBSR = 0xFE00, // keyboard status
+    MR_KBDR = 0xFE02  // keyboard data
 };
 
 // Memory Locations
@@ -42,7 +42,7 @@ enum
     R_R5,
     R_R6,
     R_R7,
-    R_PC, /* program counter */
+    R_PC, // program counter
     R_COND,
     R_COUNT
 };
@@ -87,9 +87,9 @@ void handle_interrupt(int signal)
 //Conditional Flags
 enum
 {
-    FL_POS = 1 << 0, /* P */
-    FL_ZRO = 1 << 1, /* Z */
-    FL_NEG = 1 << 2, /* N */
+    FL_POS = 1 << 0, // P
+    FL_ZRO = 1 << 1, // Z
+    FL_NEG = 1 << 2, // N
 };
 
 // Sign Extend
@@ -108,7 +108,7 @@ void update_flags(uint16_t r)
     {
         reg[R_COND] = FL_ZRO;
     }
-    else if (reg[r] >> 15) /* a 1 in the left-most bit indicates negative */
+    else if (reg[r] >> 15) // a 1 in the left-most bit indicates negative
     {
         reg[R_COND] = FL_NEG;
     }
@@ -127,17 +127,17 @@ uint16_t swap16(uint16_t x)
 // Read instr
 void read_image_file(FILE* file)
 {
-    /* the origin tells us where in memory to place the image */
+    // the origin tells us where in memory to place the image
     uint16_t origin;
     fread(&origin, sizeof(origin), 1, file);
     origin = swap16(origin);
 
-    /* we know the maximum file size so we only need one fread */
+    // we know the maximum file size so we only need one fread
     uint16_t max_read = MEMORY_MAX - origin;
     uint16_t* p = memory + origin;
     size_t read = fread(p, sizeof(uint16_t), max_read, file);
 
-    /* swap to little endian */
+    // swap to little endian
     while (read-- > 0)
     {
         *p = swap16(*p);
@@ -165,22 +165,22 @@ int read_image(const char* image_path)
 // Instruction Set
 enum
 {
-    OP_BR = 0, /* branch */
-    OP_ADD,    /* add  */
-    OP_LD,     /* load */
-    OP_ST,     /* store */
-    OP_JSR,    /* jump register */
-    OP_AND,    /* bitwise and */
-    OP_LDR,    /* load register */
-    OP_STR,    /* store register */
-    OP_RTI,    /* unused */
-    OP_NOT,    /* bitwise not */
-    OP_LDI,    /* load indirect */
-    OP_STI,    /* store indirect */
-    OP_JMP,    /* jump */
-    OP_RES,    /* reserved (unused) */
-    OP_LEA,    /* load effective address */
-    OP_TRAP    /* execute trap */
+    OP_BR = 0, // branch 
+    OP_ADD,    // add  
+    OP_LD,     // load 
+    OP_ST,     // store 
+    OP_JSR,    // jump register 
+    OP_AND,    // bitwise and 
+    OP_LDR,    // load register 
+    OP_STR,    // store register 
+    OP_RTI,    // unused 
+    OP_NOT,    // bitwise not 
+    OP_LDI,    // load indirect 
+    OP_STI,    // store indirect 
+    OP_JMP,    // jump 
+    OP_RES,    // reserved (unused) 
+    OP_LEA,    // load effective address 
+    OP_TRAP    // execute trap
 };
 
 // Memory Access
@@ -211,7 +211,7 @@ int main(int argc, const char* argv[])
     // Load Arguments
     if (argc < 2)
     {
-        /* show usage string */
+        // show usage string
         printf("lc3 [image-file1] ...\n");
         exit(2);
     }
@@ -229,18 +229,18 @@ int main(int argc, const char* argv[])
     signal(SIGINT, handle_interrupt);
     disable_input_buffering();
 
-    /* since exactly one condition flag should be set at any given time, set the Z flag */
+    // since exactly one condition flag should be set at any given time, set the Z flag
     reg[R_COND] = FL_ZRO;
 
-    /* set the PC to starting position */
-    /* 0x3000 is the default */
+    // set the PC to starting position 
+    // 0x3000 is the default 
     enum { PC_START = 0x3000 };
     reg[R_PC] = PC_START;
 
     int running = 1;
     while (running)
     {
-        /* FETCH */
+        // FETCH
         uint16_t instr = mem_read(reg[R_PC]++);
         uint16_t op = instr >> 12;
 
@@ -248,11 +248,11 @@ int main(int argc, const char* argv[])
         {
             case OP_ADD:
                 {
-                    /* destination register (DR) */
+                    // destination register (DR)
                     uint16_t r0 = (instr >> 9) & 0x7;
-                    /* first operand (SR1) */
+                    // first operand (SR1)
                     uint16_t r1 = (instr >> 6) & 0x7;
-                    /* whether we are in immediate mode */
+                    // whether we are in immediate mode
                     uint16_t imm_flag = (instr >> 5) & 0x1;
 
                     if (imm_flag)
@@ -321,12 +321,12 @@ int main(int argc, const char* argv[])
                     if (long_flag)
                     {
                         uint16_t long_pc_offset = sign_extend(instr & 0x7FF, 11);
-                        reg[R_PC] += long_pc_offset;  /* JSR */
+                        reg[R_PC] += long_pc_offset;
                     }
                     else
                     {
                         uint16_t r1 = (instr >> 6) & 0x7;
-                        reg[R_PC] = reg[r1]; /* JSRR */
+                        reg[R_PC] = reg[r1];
                     }
                 }
                 break;
@@ -340,11 +340,9 @@ int main(int argc, const char* argv[])
                 break;
             case OP_LDI:
                 {
-                    /* destination register (DR) */
                     uint16_t r0 = (instr >> 9) & 0x7;
-                    /* PCoffset 9*/
                     uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
-                    /* add pc_offset to the current PC, look at that memory location to get the final address */
+                    
                     reg[r0] = mem_read(mem_read(reg[R_PC] + pc_offset));
                     update_flags(r0);
                 }
@@ -407,7 +405,6 @@ int main(int argc, const char* argv[])
                         break;
                     case TRAP_PUTS:
                         {
-                            /* one char per word */
                             uint16_t* c = memory + reg[R_R0];
                             while (*c)
                             {
